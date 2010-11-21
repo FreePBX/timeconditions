4352ab8a8efe17fa0c61062d0b2c8f1d02a90796
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-11-21 19:46:33 +0000

    closes #4651 update module uninstalls to ignore feature codes now being deleted by module_uninstall

--- uninstall.php
+++ uninstall.php
@@ -1,12 +1,6 @@
 <?php
 
-$list = timeconditions_list(true);
-if(is_array($list)) foreach ($list as $item) {
-	$id = $item['timeconditions_id'];
-  $fcc = new featurecode('timeconditions', 'toggle-mode-'.$id);
-	$fcc->delete();
-	unset($fcc);	
-}
+// Don't bother uninstalling feature codes, now module_uninstall does it
 
 sql('DROP TABLE IF EXISTS `timegroups_groups`');
 sql('DROP TABLE IF EXISTS `timegroups_detail`');
