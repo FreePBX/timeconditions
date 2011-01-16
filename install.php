416e8e3aa89006f4fbfc29e1be3ae34c5fa64ba0
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2011-01-16 04:59:24 +0000

    add freepbx_settings to various modules and misc minor tweaks re #4740

--- install.php
+++ install.php
@@ -250,4 +250,33 @@ function timeconditions_list_forupgrade() {
 function timeconditions_set_timegroupid($id, $timegroup) {
 	sql("UPDATE timeconditions SET time = $timegroup WHERE timeconditions_id = $id;");
 }
-?>
+  // TCINTERVAL
+  //
+  $set['value'] = '60';
+  $set['defaultval'] =& $set['value'];
+  $set['readonly'] = 0;
+  $set['hidden'] = 0;
+  $set['level'] = 0;
+  $set['module'] = 'timeconditions';
+  $set['category'] = '';
+  $set['emptyok'] = 0;
+  $set['description'] = 'The polling interval in seconds used by the Time Conditions manintenace task, launched by an Asterisk call file used to update Time Conditions override states as well as keep custom device state hint values up-to-date when being used with BLF. A shorter interval will assure that BLF keys states are accurate. The interval should be less than the shortest configured span between two time condition states, so that a manual overide during such a period is properly reset when the new period starts.';
+  $set['type'] = CONF_TYPE_SELECT;
+  $set['options'] = '60, 120, 180, 240, 300, 600, 900';
+  $freepbx_conf->define_conf_setting('TCINTERVAL',$set);
+
+  // TCMAINT
+  //
+  $set['value'] = true;
+  $set['defaultval'] =& $set['value'];
+  $set['readonly'] = 0;
+  $set['hidden'] = 0;
+  $set['level'] = 0;
+  $set['module'] = 'timeconditions';
+  $set['category'] = '';
+  $set['emptyok'] = 0;
+  $set['description'] = 'If set to false, this will override the execution of the Time Conditons maintenace task launched by call files. If all the feature codes for time conditions are disabled, the maintenance task will not be launched anyhow. Setting this to false would be fairly un-common. You may want to set this temporarily if debugging a system to avoid the periodic dialplan running through the CLI that the maintenance task launches and can be distracting.';
+  $set['type'] = CONF_TYPE_BOOL;
+  $freepbx_conf->define_conf_setting('TCMAINT',$set);
+
+  $freepbx_conf->commit_conf_settings();
