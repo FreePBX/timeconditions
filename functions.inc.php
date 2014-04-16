18bd1fe1413d7b2216021a8bd63aa6e1fcfb5b6c
Anthony Lewis <tony.lewis@schmoozecom.com>
2014-04-16 16:37:05 -0500

    new license tags

--- functions.inc.php
+++ functions.inc.php
@@ -1,6 +1,8 @@
 <?php /* $Id */
 if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
-
+//	License for all code of this FreePBX module can be found in the license file inside the module directory
+//	Copyright 2013 Schmooze Com Inc.
+//
 function timeconditions_getdest($exten) {
 	return array('timeconditions,'.$exten.',1');
 }
