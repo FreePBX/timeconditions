f4a3aa9ccdc707d5cf9b7c58596c61eb4dd0b6f9
James Finstrom <jfinstrom@sangoma.com>
2015-07-06 16:56:56 -0400

    FREEPBX-9599 Add navigation (bootnav) links between time groups and time conditions

--- views/timeconditions/bootnav.php
+++ views/timeconditions/bootnav.php
@@ -1,2 +1,4 @@
 <a href="config.php?display=timeconditions" class="list-group-item <?php echo ($request['view'] == ''? 'hidden':'')?>"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Time Conditions") ?></a>
-<a href="config.php?display=timeconditions&view=form" class="list-group-item <?php echo ($request['view'] == 'form'? 'hidden':'')?>"><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Time Condition") ?></a>
+<a href="config.php?display=timegroups" class="list-group-item"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Time Groups") ?></a>
+
+<a href="config.php?display=timeconditions&view=form" class="list-group-item <?php echo ($request['view'] == 'form'? 'hidden':'')?>"><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Time Condition") ?></a
--- views/timegroups/bootnav.php
+++ views/timegroups/bootnav.php
@@ -7,6 +7,7 @@
 	<b><?php echo _("Server time:")?></b> <span id="idTime">Not received</span>
 </span>
 <a href="config.php?display=timegroups" class="list-group-item <?php echo ($request['view'] == ''? 'hidden':'')?>"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Time Groups") ?></a>
+<a href="config.php?display=timeconditions" class="list-group-item"><i class="fa fa-list"></i>&nbsp; <?php echo _("List Time Conditions") ?></a>
 <a href="config.php?display=timegroups&view=form" class="list-group-item <?php echo ($request['view'] == 'form'? 'hidden':'')?>"><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Time Group") ?></a>
 <?php
 if($request['view'] == 'form'){
