bd97c43174948db41350846e3247c5cd5f574d68
James Finstrom <jfinstrom@sangoma.com>
2016-04-20 12:05:18 -0700

    FREEPBX-12110 Correct table name for uninstall

--- uninstall.php
+++ uninstall.php
@@ -4,5 +4,5 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 // Don't bother uninstalling feature codes, now module_uninstall does it
 
 sql('DROP TABLE IF EXISTS `timegroups_groups`');
-sql('DROP TABLE IF EXISTS `timegroups_detail`');
+sql('DROP TABLE IF EXISTS `timegroups_details`');
 sql('DROP TABLE IF EXISTS `timeconditions`');
