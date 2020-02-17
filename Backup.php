17774dea5d192911ac6c61fad1872dacb325990f
Mohit Mishra <mmishra@sangoma.com>
2020-02-17 12:21:29 +0530

    FREEPBX-21190 Backup Item - Time Condition include cel table
    	Fixed. Removing the dependency of CEl. While taking backup it's not required.

--- Backup.php
+++ Backup.php
@@ -3,7 +3,6 @@ namespace FreePBX\modules\Timeconditions;
 use FreePBX\modules\Backup as Base;
 class Backup Extends Base\BackupBase{
 	public function runBackup($id,$transaction){
-		$this->addDependency('cel');
 		$this->addDependency('calendar');
 		$this->addConfigs([
 			'tables' => $this->dumpTables(),
